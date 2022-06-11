c1eb91d2f6bf46ab1ccadff2aa9d1c995f3df4a2
NisrinaNataraharja <nisrinanataraharja@gmail.com>
2022-06-11 21:41:05 +0700

    responsive auth on mobile

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import style from './auth.module.css'
+import  './auth.css'
+import style from './login.module.css'
 import { Input, Button } from '../../components/index'
 
-const Login = () => {
+
+const Login = () => {  
+
     return (
-        <div className={style.main}>
-            <div className={style["main-left"]}></div>
-            <div className={style['main-right']}>
-                <p className={style['right-title']}>Halo, Pewpeople</p>
-                <p className={style['right-sub-title']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
+        <div className={style["main-login"]}>
+            <div className="main-left"></div>
+            <div className='main-right'>
+                <p className='right-title'>Halo, Pewpeople</p>
+                <p className='right-sub-title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
                 <form>
-                    <p className='mt-5 text-muted'>Email</p>
+                    <p className='mt-5 text-muted p-0 m-0'>Email</p>
                     <Input css='inputAuth' type='email' placeholder='Masukkan alamat email' />
-                    <p className='mt-4 text-muted'>Kata Sandi</p>
+                    <p className='mt-4 text-muted p-0 m-0'>Kata Sandi</p>
                     <Input css='inputAuth' type='password' placeholder='Masukkan kata sandi' />
                     <p className='d-flex justify-content-end mt-3'>Lupa kata sandi?</p>
                     <Button btn='btn-auth' title='Masuk' />
-                    <p className='d-flex justify-content-center mt-3'>Anda belum punya akun?<span className={style["register-sub"]}>&nbsp;Daftar disini</span></p>
+                    <p className='d-flex justify-content-center mt-3'>Anda belum punya akun?<span className="register-sub">&nbsp;Daftar disini</span></p>
                 </form>
             </div>
         </div>
